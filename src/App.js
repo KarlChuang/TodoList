98de0226329987fa7ec12427b7e16baff48c3f0e
KarlChuang <karl0930@gmail.com>
2017-04-05 20:33:21 +0800

    Init TodoList

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import TodoList from './TodoList.js';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      TitleArray: [
+        {content: "Title", inputShow: false, ItemArray: [
+          {complete: false, content: "content", inputShow: false},
+          {complete: false, content: "content", inputShow: false},
+        ]},
+        {content: "Title", inputShow: false, ItemArray: [
+          {complete: false, content: "content", inputShow: false},
+          {complete: false, content: "content", inputShow: false},
+        ]},
+      ]
+    }
+  }
+  ArrayMapping = (inputArray) => {
+    let newArray = [];
+    for(let i=0; i<inputArray.length; i++) {
+      newArray.splice(i,0,(
+        <TodoList key={i.toString()}
+                  num={i}
+                  titleName={inputArray[i].content}
+                  titleShow={inputArray[i].inputShow}
+                  onChangeClick={this.handleChangeTitleClick}
+                  onChangeContent={this.handleChangeTitleContent}
+                  onDelete={this.handleTitleDelete}
+                  onChangeKeyDown={this.handleTitleChangeKeyDown}
+                  ItemArray={this.state.TitleArray[i].ItemArray}
+                  onChangeItemContent={this.handleChangeItemContent}
+                  onChangeItemKeyDown={this.handleChangeItemKeyDown}
+                  onItemChangeClick={this.handleItemChangeClick}
+                  onItemDelete={this.handleItemDelete} />
+      ))
+    }
+    return newArray;
+  }
+  handleChangeItemContent = (num1, num, value) => {
+    this.setState((prevState) => {
+      let newList = prevState.TitleArray;
+      newList[num1].ItemArray[num].content = value;
+      return {TitleArray: newList};
+    })
+  }
+  handleChangeItemKeyDown = (num1, num, event) => {
+    if (event.target.value === 13 || event.keyCode === 13) {
+      this.setState((prevState) => {
+        let newList = prevState.TitleArray;
+        newList[num1].ItemArray[num].inputShow = false;
+        return {TitleArray: newList};
+      })
+    }
+  }
+  handleItemChangeClick = (num1, num) => {
+    this.setState((prevState) => {
+      let newList = prevState.TitleArray;
+      newList[num1].ItemArray[num].inputShow = !(newList[num1].ItemArray[num].inputShow);
+      return {TitleArray: newList};
+    })
+  }
+  handleItemDelete = (num1, num) => {
+    this.setState((prevState) => {
+      let newList = prevState.TitleArray;
+      newList[num1].ItemArray.splice(num, 1);
+      return {TitleArray: newList};
+    })
+  }
+
+  handleTitleDelete = (num) => {
+    this.setState((prevState) => {
+      let newTitleArray = prevState.TitleArray;
+      newTitleArray.splice(num, 1);
+      return {TitleArray: newTitleArray};
+    });
+  }
+  handleTitleChangeKeyDown = (num, event) => {
+    if ( event.target.value === 13 || event.keyCode === 13 ) {
+      this.setState((prevState) => {
+        let newTitleArray = prevState.TitleArray;
+        newTitleArray[num].inputShow = false;
+        return {TitleArray: newTitleArray};
+      })
+    }
+  }
+  handleChangeTitleClick = (num) => {
+    this.setState((prevState) => {
+      let newTitleArray = prevState.TitleArray;
+      newTitleArray[num].inputShow = !(newTitleArray[num].inputShow);
+      return {TitleArray: newTitleArray};
+    })
+  }
+  handleChangeTitleContent = (num, value) => {
+    this.setState((prevState) => {
+      let newTitleArray = prevState.TitleArray;
+      newTitleArray[num].content = value;
+      return {TitleArray: newTitleArray};
+    })
+  }
+  render() {
+    let newTodoList = this.ArrayMapping(this.state.TitleArray);
+    const ListNumber = this.state.TitleArray.length;
+
+    return (
+      <div className="App">
+        <h1>TODO-LIST ({(ListNumber>1) ? ListNumber + " Lists" : ListNumber+ " List"})</h1>
+        <div>
+          {newTodoList}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import './TodoList.css'
+
+class TodoList extends Component {
+
+  ArrayMapping = (inputArray) => {
+    let newArray = [];
+    for(let i=0; i<inputArray.length; i++) {
+      newArray.splice(i, 0, (
+        <TodoItem key={i.toString()} 
+                  num={i}
+                  complete={inputArray[i].complete}
+                  content={inputArray[i].content}
+                  inputShow={inputArray[i].inputShow}
+                  onChangeContent={(num, value) => this.props.onChangeItemContent(this.props.num, num, value)}
+                  onChangeKeyDown={(num, event) => this.props.onChangeItemKeyDown(this.props.num, num, event)}
+                  onChangeClick={(num) => this.props.onItemChangeClick(this.props.num, num)}
+                  onDelete={(num) => this.props.onItemDelete(this.props.num, num)} />
+      ))
+    }
+    return newArray;
+  }
+  
+  render() {
+    const num = this.props.num;
+    // Title
+    const TitleName = this.props.titleName;
+    const titleInputShow = this.props.titleShow;
+    const handleChangeTitleClick = this.props.onChangeClick;
+    const handleChangeTitleContent = this.props.onChangeContent;
+    const handleTitleDelete = this.props.onDelete
+    const handleChangeTitleKeyDown = this.props.onChangeKeyDown
+    // Items
+    let newTodoItemList = this.ArrayMapping(this.props.ItemArray);
+    const ItemNumber = this.props.ItemArray.length;
+
+    return (
+      <div className="TodoList">
+        <TodoListTitle titleName={TitleName}
+                        inputShow={titleInputShow}
+                        onChangeClick={() => handleChangeTitleClick(num)}
+                        onChangeContent={(value) => handleChangeTitleContent(num, value)}
+                        onChangeKeyDown={(event) => handleChangeTitleKeyDown(num, event)}
+                        onDelete={() => handleTitleDelete(num)} />
+        <div className="TodoItemList">
+          {newTodoItemList}
+        </div>
+        <div className="ToolBar">{(ItemNumber>1) ? ItemNumber + " Items" : ItemNumber + " Item"}</div>
+      </div>
+    );
+  }
+}
+
+class TodoListTitle extends Component {
+  render() {
+    const titleName = this.props.titleName;
+    const inputShow = this.props.inputShow;
+    const handleChangeClick = this.props.onChangeClick;
+    const handleChangeContent = this.props.onChangeContent;
+    const handleChangeKeyDown = this.props.onChangeKeyDown;
+    const handleDelete = this.props.onDelete;
+    return (
+      <div className="TodoListTitle">
+        <div className="title">{titleName}</div>
+        <img className="delete" src={require("./pic/delete.png")} alt="delete"
+              onClick={handleDelete} />
+        <img className="write" src={require("./pic/write.png")} alt="write"
+              onClick={handleChangeClick} />
+        <br/> 
+        {(inputShow) ? (
+          <input type="text" className="Input" 
+                value={titleName} 
+                onChange={(event) => handleChangeContent(event.target.value)} 
+                onKeyDown={(event) => handleChangeKeyDown(event)} /> 
+        ): null}
+      </div>
+    )
+  }
+}
+
+class TodoItem extends Component {
+  render() {
+    const num = this.props.num;
+    const complete = this.props.complete;
+    const content = this.props.content;
+    const inputShow = this.props.inputShow;
+    const handleChangeContent = this.props.onChangeContent;
+    const handleChangeKeyDown = this.props.onChangeKeyDown;
+    const handleChangeClick = this.props.onChangeClick;
+    const handleDelete = this.props.onDelete;
+    return (
+      <div className="TodoItem" >
+        <div className="button" />
+        <div className="thing">{content}</div>
+        <img className="delete" src={require("./pic/delete.png")} alt="delete"
+              onClick={() => handleDelete(num)} />
+        <img className="write" src={require("./pic/write.png")} alt="write"
+              onClick={() => handleChangeClick(num)} />
+        <br/> 
+        {(inputShow) ? (
+          <input type="text" className="Input"
+                value={content} 
+                onChange={(event) => handleChangeContent(num , event.target.value)} 
+                onKeyDown={(event) => handleChangeKeyDown(num, event)} /> 
+          ) : null}
+      </div>
+    )
+  }
+}
+
+export default TodoList;
