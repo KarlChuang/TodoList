e5169de2f511a8c94c4739a33cb9b68dbec036e9
KarlChuang <karl0930@gmail.com>
2017-04-07 12:28:06 +0800

    use eslint --fix to debug

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react'
-import './TodoList.css'
+import React, { Component } from 'react';
+import './TodoList.css';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.ArrayMapping = this.ArrayMapping.bind(this);
+  }
 
-  ArrayMapping = (inputArray, showStatus) => {
-    let newArray = [];
-    for(let i=0; i<inputArray.length; i++) {
+  ArrayMapping(inputArray, showStatus) {
+    const newArray = [];
+    for (let i = 0; i < inputArray.length; i += 1) {
       newArray.splice(i, 0, (
-        <TodoItem key={i.toString()} 
-                  num={i}
-                  complete={inputArray[i].complete}
-                  content={inputArray[i].content}
-                  inputShow={inputArray[i].inputShow}
-                  onChangeContent={(num, value) => this.props.onChangeItemContent(this.props.num, num, value)}
-                  onChangeKeyDown={(num, event) => this.props.onChangeItemKeyDown(this.props.num, num, event)}
-                  onChangeClick={(num) => this.props.onItemChangeClick(this.props.num, num)}
-                  onDelete={(num) => this.props.onItemDelete(this.props.num, num)}
-                  onCompleteClick={(num) => this.props.onItemCompleteClick(this.props.num, num)} />
-      ))
+        <TodoItem
+          key={i.toString()}
+          num={i}
+          complete={inputArray[i].complete}
+          content={inputArray[i].content}
+          inputShow={inputArray[i].inputShow}
+          onChangeContent={(num, value) => this.props.onChangeItemContent(this.props.num, num, value)}
+          onChangeKeyDown={(num, event) => this.props.onChangeItemKeyDown(this.props.num, num, event)}
+          onChangeClick={num => this.props.onItemChangeClick(this.props.num, num)}
+          onDelete={num => this.props.onItemDelete(this.props.num, num)}
+          onCompleteClick={num => this.props.onItemCompleteClick(this.props.num, num)} 
+        />
+      ));
     }
-    if (showStatus !== "All")
-    {
-      for(let i=inputArray.length-1; i>=0; i--) { 
-        if (inputArray[i].complete !== (showStatus==="Complete")) {
+    if (showStatus !== 'All') {
+      for (let i = inputArray.length - 1; i >= 0; i -= 1) {
+        if (inputArray[i].complete !== (showStatus === 'Complete')) {
           newArray.splice(i, 1);
         }
       }
     }
     return newArray;
   }
-  
+
   render() {
     const num = this.props.num;
     // Title
@@ -40,15 +45,15 @@ class TodoList extends Component {
     const handleTitleDelete = this.props.onDelete;
     const handleChangeTitleKeyDown = this.props.onChangeKeyDown;
     // Items
-    let newTodoItemList = this.ArrayMapping(this.props.ItemArray, this.props.showStatus);
+    const newTodoItemList = this.ArrayMapping(this.props.ItemArray, this.props.showStatus);
     const ItemNumber = this.props.ItemArray.length;
     let CompleteItemNumber = 0;
     let UndoItemNumber = 0;
-    for (let i=0; i<ItemNumber; i++) {
+    for (let i = 0; i < ItemNumber; i += 1) {
       if (this.props.ItemArray[i].complete === true) {
-        CompleteItemNumber++;
+        CompleteItemNumber += 1;
       } else {
-        UndoItemNumber++;
+        UndoItemNumber += 1;
       }
     }
     const handleAddButtonClick = this.props.onAddButtonClick;
@@ -58,22 +63,25 @@ class TodoList extends Component {
 
     return (
       <div className="TodoList">
-        <TodoListTitle titleName={TitleName}
-                        inputShow={titleInputShow}
-                        onChangeClick={() => handleChangeTitleClick(num)}
-                        onChangeContent={(value) => handleChangeTitleContent(num, value)}
-                        onChangeKeyDown={(event) => handleChangeTitleKeyDown(num, event)}
-                        onDelete={() => handleTitleDelete(num)} />
+        <TodoListTitle
+          titleName={TitleName}
+          inputShow={titleInputShow}
+          onChangeClick={() => handleChangeTitleClick(num)}
+          onChangeContent={value => handleChangeTitleContent(num, value)}
+          onChangeKeyDown={event => handleChangeTitleKeyDown(num, event)}
+          onDelete={() => handleTitleDelete(num)}
+        />
         <div className="TodoItemList">
           {newTodoItemList}
-          <div className="AddButton"
-                onClick={() => handleAddButtonClick(num)}
+          <div
+            className="AddButton"
+            onClick={() => handleAddButtonClick(num)}
           >+</div>
         </div>
         <div className="Tool">
-          <div className={(this.props.showStatus==="All")?"ToolBarDown":"ToolBar"} onClick={() => {handleAllButtonClick(num)}}>All ( {ItemNumber} )</div>
-          <div className={(this.props.showStatus==="Complete")?"ToolBarDown":"ToolBar"} onClick={() => {handleCompleteButtonClick(num)}}>Complete ( {CompleteItemNumber} )</div>
-          <div className={(this.props.showStatus==="Undo")?"ToolBarDown":"ToolBar"} onClick={() => {handleUndoButtonClick(num)}}>Undo ( {UndoItemNumber} )</div>
+          <div className={(this.props.showStatus === 'All') ? 'ToolBarDown' : 'ToolBar'} onClick={() => { handleAllButtonClick(num); }}>All ( {ItemNumber} )</div>
+          <div className={(this.props.showStatus === 'Complete') ? 'ToolBarDown' : 'ToolBar'} onClick={() => { handleCompleteButtonClick(num); }}>Complete ( {CompleteItemNumber} )</div>
+          <div className={(this.props.showStatus === 'Undo') ? 'ToolBarDown' : 'ToolBar'} onClick={() => { handleUndoButtonClick(num); }}>Undo ( {UndoItemNumber} )</div>
         </div>
       </div>
     );
@@ -91,18 +99,24 @@ class TodoListTitle extends Component {
     return (
       <div className="TodoListTitle">
         {(inputShow) ? (
-          <input type="text" className="Input" 
-                value={titleName} 
-                onChange={(event) => handleChangeContent(event.target.value)} 
-                onKeyDown={(event) => handleChangeKeyDown(event)} /> 
+          <input
+            type="text" className="Input"
+            value={titleName}
+            onChange={event => handleChangeContent(event.target.value)}
+            onKeyDown={event => handleChangeKeyDown(event)}
+          />
         ) : (<div className="title">{titleName}</div>)}
-        <img className="delete" src={require("./pic/delete.png")} alt="delete"
-              onClick={handleDelete} />
-        <img className="write" src={require("./pic/write.png")} alt="write"
-              onClick={handleChangeClick} />
-        <br/> 
+        <img
+          className="delete" src={require('./pic/delete.png')} alt="delete"
+          onClick={handleDelete}
+        />
+        <img
+          className="write" src={require('./pic/write.png')} alt="write"
+          onClick={handleChangeClick}
+        />
+        <br />
       </div>
-    )
+    );
   }
 }
 
@@ -119,19 +133,25 @@ class TodoItem extends Component {
     const handleCompleteClick = this.props.onCompleteClick;
     return (
       <div className="TodoItem" >
-        <div className={(complete)?"buttonComplete":"button"} onClick={() => handleCompleteClick(num)}/>
+        <div className={(complete) ? 'buttonComplete' : 'button'} onClick={() => handleCompleteClick(num)} />
         {(inputShow) ? (
-          <input type="text" className="Input"
-                value={content} 
-                onChange={(event) => handleChangeContent(num , event.target.value)} 
-                onKeyDown={(event) => handleChangeKeyDown(num, event)} /> 
-          ) : (<div className={(complete)?"thingComplete":"thing"}>{content}</div>) }
-        <img className="delete" src={require("./pic/delete.png")} alt="delete"
-              onClick={() => handleDelete(num)} />
-        <img className="write" src={require("./pic/write.png")} alt="write"
-              onClick={() => handleChangeClick(num)} />
+          <input
+            type="text" className="Input"
+            value={content}
+            onChange={event => handleChangeContent(num, event.target.value)}
+            onKeyDown={event => handleChangeKeyDown(num, event)}
+          />
+          ) : (<div className={(complete) ? 'thingComplete' : 'thing'}>{content}</div>) }
+        <img
+          className="delete" src={require('./pic/delete.png')} alt="delete"
+          onClick={() => handleDelete(num)}
+        />
+        <img
+          className="write" src={require('./pic/write.png')} alt="write"
+          onClick={() => handleChangeClick(num)}
+        />
       </div>
-    )
+    );
   }
 }
 
